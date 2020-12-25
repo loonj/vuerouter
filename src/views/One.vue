533b03d35f0cb133f69f24bdc6bd656265fee0f0
loonj <loonj@163.com>
2020-12-25 12:13:17 +0800

    可以跑起来的一个版本

---
+++ src/views/One.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h3>第一个页面</h3>
+        <img class="headerimg" src="https://www.zhanx.cn/sfmyimages/banner/banner4.jpg" alt="" >
+        <ul>
+            <li v-for="item in mineArr" class="mineItem" :key="item.lable" @click="itemClick(item)">
+                <span class="mineTitle">{{item.label}}</span>
+                <i class="cubeic-arrow"></i>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            mineArr:[
+                {label:'商品收藏'},
+                {label:'我的足迹'},
+                {label:'店铺收藏'},
+                {label:'我的订单'},
+                {label:'退出',type:'exit'},
+            ]
+        }
+    },
+    methods:{
+        itemClick(item){
+            if(item.type=='exit'){
+                // this.$store.commit('setToken','');
+                // sessionStorage.removeItem('token');
+                // this.$router.push({path:"/login"});
+                console.log("退出。。。。。");
+            }
+        }
+
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .mineItem
+        font-size 14px
+        text-align left 
+        height 50px
+        line-height 50px
+        padding-left 5%
+        border-bottom 1px solid #eee
+        .mineTitle
+            display inline-block
+            width 90%
+    .headerimg
+        height 150px
+        width 100%
+
+</style>
---
+++ src/views/Two.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>第二个页面</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Two'
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
